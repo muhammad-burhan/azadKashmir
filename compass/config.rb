e03838feac2ce0875b85cb16ad387a01c5291ff5
Burhan Noman <mburhannoman@gmail.com>
2016-11-25 00:39:17 +0500

    Completed the locations and destinations

--- compass/config.rb
+++ compass/config.rb
@@ -1,10 +1,10 @@
-equire 'compass/import-once/activate'
+require 'compass/import-once/activate'
 # Require any additional compass plugins here.
 
 http_path 		= "../"
 css_dir 		= "../css"
 sass_dir 		= "sass"
-images_dir 		= "../img"
+images_dir 		= "../images"
 javascripts_dir = "../js"
 fonts_dir		= "../fonts"
 
