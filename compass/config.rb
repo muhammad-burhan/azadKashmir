87ed12d3decf218c8779c078e4cdf17a42273ee2
Burhan Noman <mburhannoman@gmail.com>
2016-11-24 22:30:45 +0500

    setting up the project

---
+++ compass/config.rb
@@ -0,0 +1,12 @@
+equire 'compass/import-once/activate'
+# Require any additional compass plugins here.
+
+http_path 		= "../"
+css_dir 		= "../css"
+sass_dir 		= "sass"
+images_dir 		= "../img"
+javascripts_dir = "../js"
+fonts_dir		= "../fonts"
+
+output_style 	= :compressed
+line_comments 	= false
